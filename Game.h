4df1f0b99934757b5625dfc95af952138891c14e
Yu-Xiao <ma.yuxiao1003@gmail.com>
2015-09-09 16:31:14 -0400

    UI setup

---
+++ Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  The Flappy Saga
+//
+//  Created by Yuxiao Ma on 2015-09-09.
+//  Copyright (c) 2015 Yuxiao Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Game : UIViewController
+
+@end
---
+++ Game.m
@@ -0,0 +1,37 @@
+//
+//  Game.m
+//  The Flappy Saga
+//
+//  Created by Yuxiao Ma on 2015-09-09.
+//  Copyright (c) 2015 Yuxiao Ma. All rights reserved.
+//
+
+#import "Game.h"
+
+@interface Game ()
+
+@end
+
+@implementation Game
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
